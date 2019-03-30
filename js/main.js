4d7d22737c11c82d9d0525908acd2252ea0c554f
Alfredo-Git <algonzalezramirez@gmail.com>
2019-03-30 11:34:24 +0100

    added video selector

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ const video = document.querySelector('video');
 const canvas = window.canvas = document.querySelector('canvas');
 canvas.width = 480;
 canvas.height = 360;
+var videoSelect = document.querySelector('select#videoSource');
 
 const button = document.querySelector('button');
 button.onclick = function() {
@@ -21,10 +22,43 @@ button.onclick = function() {
   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
 };
 
-const constraints = {
-  audio: false,
-  video: true
-};
+navigator.mediaDevices.enumerateDevices()
+  .then(gotDevices).then(getStream).catch(handleError);
+
+videoSelect.onchange = getStream;
+
+function gotDevices(deviceInfos) {
+  for (var i = 0; i !== deviceInfos.length; ++i) {
+    var deviceInfo = deviceInfos[i];
+    var option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || 'camera ' +
+        (videoSelect.length + 1);
+      videoSelect.appendChild(option);
+    } else {
+      console.log('Found one other kind of source/device: ', deviceInfo);
+    }
+  }
+}
+
+function getStream() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(function(track) {
+      track.stop();
+    });
+  }
+
+  var constraints = {
+    audio: false,
+    video: {
+      deviceId: {exact: videoSelect.value}
+    }
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);
+}
 
 function handleSuccess(stream) {
   window.stream = stream; // make stream available to browser console
@@ -34,5 +68,3 @@ function handleSuccess(stream) {
 function handleError(error) {
   console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
 }
-
-navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
